e468e72b91a3cc882926ab515967469ca8fb7bb7
Gilvan borges dos santos <164768678+gilvan-Borges@users.noreply.github.com>
2024-10-19 12:38:26 -0300

    Update script.sql

--- src/main/java/br/com/cotiinformatica/sql/script.sql
+++ src/main/java/br/com/cotiinformatica/sql/script.sql
@@ -1,10 +1,1 @@
 
-CREATE TABLE contato(
-	id	UUID			PRIMARY KEY,
-	nome 		VARCHAR(150) 	NOT NULL,
-	cpf			VARCHAR(14)		NOT NULL UNIQUE,
-	telefone 	VARCHAR(20) 	NOT NULL,
-	email	 	VARCHAR(50) 	NOT NULL
-);
-
-DESC contato;
